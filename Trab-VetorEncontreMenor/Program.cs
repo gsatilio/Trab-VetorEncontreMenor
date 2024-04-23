8cb99460a22986a989de8cc6889d1a716fc977ae
Guilherme <g.satilio@gmail.com>
2024-04-23 15:39:17 -0300

    novas tratativas

--- Trab-VetorEncontreMenor/Program.cs
+++ Trab-VetorEncontreMenor/Program.cs
@@ -1,12 +1,22 @@
 ﻿// faça um programa que leia 5 valores e encontre o menor
-int aux = 0, size = 10;
+using System.Runtime.Intrinsics.X86;
 
-int[] numeros = new int[size];
+int aux = 0, size = 10, repetidos = 0, sizefinal = 0;
+
+int[] numeros = new int[size]; 
+//int[] numeros = new int[] { 2, 3, 7, 4, 4, 0, 234, 234, 1, 5}; ; 
 int[] ordenado = new int[size];
+int[] ordenado_norepeat = new int[size];
+int[] ordenado_norepeatback = new int[size];
 
+
+// pega numeros
 for(int i = 0; i < size; i++)
 {
-    numeros[i] = new Random().Next(0, 100);
+    //numeros[i] = new Random().Next(0, 100);
+    numeros[i] = int.Parse(Console.ReadLine());
+    ordenado[i] = numeros[i];
+    ordenado_norepeat[i] = numeros[i];
 }
 
 for(int i =  0; i < size; i++)
@@ -14,26 +24,86 @@ for(int i =  0; i < size; i++)
     ordenado[i]  = numeros[i]; // copia os dados do vetor NUMEROS para o vetor ORDENADO
 }
 
+// ordena normal
 for (int i = 0; i < size; i++)
 {
-    for (int j = i+1; j < size; j++)
+    for (int j = i + 1; j < size; j++)
     {
+        // ordenado normal
         if (ordenado[i] > ordenado[j])
         {
             aux = ordenado[i];
             ordenado[i] = ordenado[j];
             ordenado[j] = aux;
         }
+
+        // ordenado sem repetir
+        if (ordenado_norepeat[i] > ordenado_norepeat[j])
+        {
+            aux = ordenado_norepeat[i];
+            ordenado_norepeat[i] = ordenado_norepeat[j];
+            ordenado_norepeat[j] = aux;
+        }
+        else if (ordenado_norepeat[i] == ordenado_norepeat[j])
+        {
+            ordenado_norepeat[i] = 0;
+            repetidos++;
+        }
+
     }
 }
 
+// ordena novamente os repetidos, para começar do zero
+for (int i = 0; i < size; i++)
+{
+    for (int j = i + 1; j < size; j++)
+    {
+        // ordenado sem repetir
+        if (ordenado_norepeat[i] > ordenado_norepeat[j])
+        {
+            aux = ordenado_norepeat[i];
+            ordenado_norepeat[i] = ordenado_norepeat[j];
+            ordenado_norepeat[j] = aux;
+        }
+        else if (ordenado_norepeat[i] == ordenado_norepeat[j])
+        {
+            ordenado_norepeat[i] = 0;
+        }
+
+    }
+}
+
+
+sizefinal = size - repetidos;
+
+Console.WriteLine("\nNumeros informados:");
 for (int i = 0; i < size; i++)
 {
     Console.Write(numeros[i] + " ");
 }
 
-Console.WriteLine();
-
+Console.WriteLine("\nNumeros ordenados:");
 for (int i = 0; i < size; i++){
     Console.Write(ordenado[i] + " ");
 }
+
+Console.WriteLine("\nNumeros ordenados sem repetir:");
+for (int i = repetidos; i < size; i++)
+{
+    Console.Write(ordenado_norepeat[i] + " ");
+}
+
+if(sizefinal == 10)
+{
+    sizefinal = 9;
+}
+Console.WriteLine("\nNumeros ordenados sem repetir ao contrario:");
+//for (int i = sizefinal+1; i >= repetidos; i--)
+// Exemplo SIZE = 10 numeros, REPETIDOS = 2 repetiu 2, entao I = 10 + 2 - 1 (pois pro vetor eh uma casa a menos)
+for(int i = (sizefinal); i >= 0 + repetidos; i--)
+{
+    Console.Write(ordenado_norepeat[i] + " ");
+}
+
+
+Console.WriteLine();
